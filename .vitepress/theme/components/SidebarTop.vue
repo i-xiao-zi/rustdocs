238387404d6916901af50ec8fcd53a12a0a95c6e
ixiaozi <xiaozi@ixiaozi.cn>
2024-01-29 19:47:57 +0800

    *

--- .vitepress/theme/components/SidebarTop.vue
+++ .vitepress/theme/components/SidebarTop.vue
@@ -4,29 +4,30 @@
 
 <script setup lang='ts'>
 import { useData } from 'vitepress'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
-const { theme } = useData()
+const { theme, page } = useData()
 
 const title = ref('title')
-
-for (const sidebarKey in theme.value.sidebar) {
-  if (document.location.pathname.indexOf(sidebarKey) > -1) {
-    for (const navValue of theme.value.nav) {
-      if (Object.keys(navValue).indexOf('link') > -1) {
-        if (navValue.link.indexOf(sidebarKey) > -1) {
-          title.value = navValue
-        }
-      } else if (Object.keys(navValue).indexOf('activeMatch') > -1) {
-        for (const item of navValue.items) {
-          if (Object.keys(item).indexOf('link') > -1 && item.link.indexOf(sidebarKey) > -1) {
-            title.value = item.text
+onMounted(() => {
+  for (const sidebarKey in theme.value.sidebar) {
+    if (`/${page.value.relativePath}`.indexOf(sidebarKey) > -1) {
+      for (const navValue of theme.value.nav) {
+        if (Object.keys(navValue).indexOf('link') > -1) {
+          if (navValue.link.indexOf(sidebarKey) > -1) {
+            title.value = navValue
+          }
+        } else if (Object.keys(navValue).indexOf('activeMatch') > -1) {
+          for (const item of navValue.items) {
+            if (Object.keys(item).indexOf('link') > -1 && item.link.indexOf(sidebarKey) > -1) {
+              title.value = item.text
+            }
           }
         }
       }
     }
   }
-}
+})
 </script>
 
 <style scoped>
--- src/askama/template-syntax.md
+++ src/askama/template-syntax.md
@@ -6,11 +6,51 @@ titleTemplate: Askama 非官方不完全文档
 # 模板语法
 
 ### 1. 变量
-
 顶级模板变量由模板的上下文类型定义。您可以使用点(`.`)来访问变量的属性或方法。
 从变量中读取数据受通常的`借用`政策的约束。 例如，
-- `{{ name }}`从模板上下文中获取字段`name`，
-- `{{ user.name }}`从模板上下文中获取`user`字段的`name`字段。
-
+- <span v-pre>`{{ name }}`</span>从模板上下文中获取字段`name`
+  ```rust
+  use askama::Template;
+  
+  #[derive(Template)]
+  #[template(source="Hello {{name}}", ext="txt")]
+  struct HelloTemplate {
+      name: String,
+  }
+  fn main() {
+      println!("{}", HelloTemplate { name: "world".into()}.render().unwrap());
+  }
+  ```
+- <span v-pre>`{{ user.name }}`</span>从模板上下文中获取`user`字段的`name`字段。
+  ```rust
+  use askama::Template;
+  
+  struct  User {
+      name: String
+  }
+  #[derive(Template)]
+  #[template(source="Hello {{user.name}}", ext="txt")]
+  struct UserTemplate {
+      user: User
+  }
+  fn main() {
+      println!("{}", UserTemplate { user: User { name: "world".into() }}.render().unwrap())
+  } 
+  ```
 ### 2. 常量
-a
+您可以使用`Rust`代码中定义的常量。例如，
+  ```rust
+  use askama::Template;
+  
+  pub const USER_NAME: &'static str = "world";
+  
+  #[derive(Template)]
+  #[template(source="Hello {{crate::USER_NAME}}", ext="txt")]
+  struct HelloTemplate {}
+  fn main() {
+      println!("{}", HelloTemplate {}.render().unwrap());
+  }
+  ```
+### 3. 模板内声明变量(`Assignments`)
+在代码块内，您还可以声明变量或为变量赋值。其他模板无法导入。
+
